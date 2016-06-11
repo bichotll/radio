a70db16d164e932e80f1bed3e7a167400ec307aa
bichotll <jaume@bic.cat>
2016-06-11 20:48:23 +0100

    config n services created

---
+++ app/scripts/services/radio.js
@@ -0,0 +1,21 @@
+var radioConfig = require('../config/radio');
+
+export default new function () {
+    /**
+     * Get all songs
+     * @return promise
+     */
+    this.getSongs = function() {
+        console.log(radioConfig.apiUrl + 'songs');
+        return $.get(radioConfig.apiUrl + 'songs');
+    };
+
+    /**
+     * Get song by id
+     * @param id
+     * @return promise
+     */
+    this.getSong = function (id) {
+        return $.get(radioConfig + 'songs/' + id);
+    };
+};
